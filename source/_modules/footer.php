85522ed9aaaa976bf71867c7656236ec9af9fc11
MahinulA <himahinulabid@gmail.com>
2022-07-16 14:12:28 +0600

    added reuseable footer and initialized service page

---
+++ source/_modules/footer.php
@@ -0,0 +1,36 @@
+<footer class="bound">
+        <div class="footer-flex-container display-flex d-flex-space-between">
+            <div class="footer-flex-item">
+                <h1 class="big-heading"> <?php echo $footer_heading_text; ?> </h1>
+                <a href="#" class="footer-link-take-action normal-heading"> <?php echo $footer_main_btn_text; ?> →</a>
+                <a class="footer-email " href="mailto:contact@debukstudio.com">contact@debukstudio.com</a>
+            </div>
+
+            <div class="footer-flex-item" >
+                <div class="fist_link-list">
+                    <a href="#" class="display-block footer-links-a">Home →</a>
+                    <a href="#" class="display-block footer-links-a">About →</a>
+                    <a href="#" class="display-block footer-links-a">Project →</a>
+                    <a href="#" class="display-block footer-links-a">Services →</a>
+                </div>
+
+                <div class="second_link-list">
+                    <p class="footer-links-a comming-soon-text">Coming Soon</p>
+                    <a href="#" class="display-block footer-links-a">Shop →</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="footer-copyright display-flex d-flex-space-between">
+            <div class="footer-copyright-item">©2022 debukstudio</div>
+            <div class="footer-copyright-item">
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                
+            </div>
+        </div>
+    </footer>
+
+    <!-- Let’s talk about your next big brand. -->
+    <!-- Take action -->
--- source/_modules/navigation_bar.php
+++ source/_modules/navigation_bar.php
@@ -5,9 +5,60 @@
         <span class="logo-text display-inline-block">debuk studio</span>
     </div>
     <div class="nav-flex-item display-flex">
-        <div class="nav-menu-item"><a href="#">Services</a> </div>
-        <div class="nav-menu-item"><a href="#">Project</a> </div>
-        <div class="nav-menu-item"><a href="#">Contact</a> </div>
+
+        <?php
+            $nav_first_link_text; $nav_first_link_path; 
+            $nav_second_link_text; $nav_second_link_path; 
+            $nav_third_link_text; $nav_third_link_path; 
+        ?>
+        <div class="nav-menu-item"><a href=<?php echo $nav_first_link_path; ?> > <?php echo $nav_first_link_text; ?> </a> </div>
+        <div class="nav-menu-item"><a href=<?php echo $nav_second_link_path; ?>> <?php echo $nav_second_link_text; ?> </a> </div>
+        <div class="nav-menu-item"><a href=<?php echo $nav_third_link_path; ?> > <?php echo $nav_third_link_text; ?> </a> </div>
         <div class="nav-menu-item"><img src=<?php echo $path_to_hamburger_icon; ?> alt="hamburger" style="cursor: pointer;"></div>
     </div>
 </nav>
+
+
+
+<!-- This nav is hamberger navigation -->
+<div class="hamberger-navigation-container bound">
+    <header class="display-flex d-flex-space-between">
+        <div class="ham-header-navigation--item">
+            <div class="ham-header-nav__first-section">
+                <a class="ham-header-nav__first-section__links display-block" href="#">HOME</a>
+                <a class="ham-header-nav__first-section__links display-block" href="#">ABOUT</a>
+                <a class="ham-header-nav__first-section__links display-block" href="#">SERVICES</a>
+                <a class="ham-header-nav__first-section__links display-block" href="#">PROJECTS</a>
+                <a class="ham-header-nav__first-section__links display-block" href="#">CONTACT</a>
+            </div>
+
+            <div class="ham-header-nav__second-section">
+                <p class="ham-header-nav__first-section__text">COMING SOON</p>
+                <a class="ham-header-nav__first-section__links display-block" href="#">CONTACT</a>
+                <a class="ham-header-nav__second-section__links display-block" href="#">POSTERS</a>
+            </div>
+        </div>
+
+        <div class="ham-header-navigation--item" style="position: relative;">
+            <img src=<?php echo $path_to_themeChange_btn; ?> alt="" height="56" width="56" style= "cursor:pointer;">
+            <div class="display-block cookie-ham-notif">
+                <p class="cookie-ham-para">
+                    This website uses
+                    cookies for the better experience
+                </p>
+                
+                <div class="close-notif-container">
+                    <img src=<?php echo $path_to_cross_btn; ?> alt="" class="ham-close-notification-btn">
+                </div>
+
+            </div>
+        </div>
+    </header>
+
+    <section class="display-flex d-flex-space-between ham-nav-footer">
+        <div class="ham-footer-flex-item"><i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-twitter"></i></div>
+        <div class="ham-footer-flex-item">
+            <button class="close-ham-bar paragraph-text">Close</button>
+        </div>
+    </section>
+</div>
