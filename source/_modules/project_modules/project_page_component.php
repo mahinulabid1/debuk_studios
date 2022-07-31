232028c3eeaf65a5ef76f6651e8cb5ce86a819f1
MahinulA <himahinulabid@gmail.com>
2022-07-31 12:38:58 +0600

    organized project_page_component

--- source/_modules/project_modules/project_page_component.php
+++ source/_modules/project_modules/project_page_component.php
@@ -1,6 +1,5 @@
 <header>
 
-
     <!-- REUSEABLE NAVIGATION BAR -->
     <?php
     // SETTING UP REUSEABLE NAVIGATION BAR AND ITS PATH
@@ -12,37 +11,8 @@
 
     $nav_third_link_text = "Contact";
     $nav_third_link_path = "./contact.php";
-
-    $nav_bar_logolink =  "./source/media/logoicon-white.png";
-
-    $path_to_themeChange_btn = "./source/media/ThemeChange_btn.png";
-    $path_to_cross_btn = "./source/media/cross-btn.png";
-
-    $path_to_hamburger_icon = "./source/media/Hamburger-white.png";
-
     include "./source/_modules/navigation_bar.php";
     ?>
-
-    <script>
-        //just change the source through javascipt
-        let hamberger_icon_image = document.querySelector(".ham-berger-icon-img");
-        let website_logo_icon = document.querySelector(".website-logo-icon");
-        let windowSize = window.screen.width;
-        if (windowSize < 834) {
-            // hamberger_icon_image.src = "./source/media/Hamburger.png";
-            // website_logo_icon.src = './source/media/logoicon.png';
-        }
-    </script>
-
-
-
-
-
-
-
-
-
-
 
 </header>
 
@@ -52,8 +22,8 @@ $page_main_heading;
 $page_sub_heading_one;
 $page_sub_heading_two;
 $page_main_para;
+
 // video file source link
-$video_poster;
 $source_to_video_file;
 $source_to_video_file_tablet_version;
 $source_to_video_file_mobile_version;
@@ -69,28 +39,33 @@ $source_to_image_2;
 <!-- hero video -->
 <div class="hero-video-container">
     <video class="hero-video" preload="auto" autoplay muted loop playsinline>
-        <!-- poster="./source/media/luxio/image3.webp" -->
         <!-- CODE WILL BE INJECTED BY THE SCRIPT BELOW -->
-
-
     </video>
+
     <script>
         // SINCE CHROME DOESN'T SUPPORT THE VIDEO SOURCE MEDIA QUERIES, THIS SCRIPT IS CREATED TO CHANGE VIDEO SOURCEC ACCORDING TO THE VIEW WIDTH
         var video = document.querySelector(".hero-video");
-        var WindowWidth = window.screen.width;
 
-        if (WindowWidth < 835 && WindowWidth > 600) {
-            //TABLET VERSION VIDEO
-            video.innerHTML = `<source src=<?php echo $source_to_video_file_tablet_version; ?> type='video/webm' >`;
-        } else if (WindowWidth < 600) {
-            //MOBILE VERSION VIDEO
-            video.innerHTML = `<source src=<?php echo $source_to_video_file_mobile_version; ?> type='video/mp4' >`;
-        } else {
-            //BIG SCREEN VIDEO
-            video.innerHTML = `<source src=<?php echo $source_to_video_file; ?> type='video/mp4' >`;
+        const Project_changeVideoSource = () => {
+            var WindowWidth = window.screen.width;
+            if (WindowWidth < 835 && WindowWidth > 600) {
+                //TABLET VERSION VIDEO
+                video.innerHTML = `<source src=<?php echo $source_to_video_file_tablet_version; ?> type='video/webm' >`;
+            } else if (WindowWidth < 600) {
+                //MOBILE VERSION VIDEO
+                video.innerHTML = `<source src=<?php echo $source_to_video_file_mobile_version; ?> type='video/mp4' >`;
+            } else {
+                //BIG SCREEN VIDEO
+                video.innerHTML = `<source src=<?php echo $source_to_video_file; ?> type='video/mp4' >`;
+            }
         }
 
-        window.addEventListener("load", ()=>{
+        window.addEventListener("load", () => {
+            Project_changeVideoSource();
+            video.play();
+        });
+        window.addEventListener("resize", () => {
+            Project_changeVideoSource();
             video.play();
         });
     </script>
@@ -133,9 +108,4 @@ $source_to_image_2;
         </div>
     </div>
 
-
-
 </section>
-
-
-
